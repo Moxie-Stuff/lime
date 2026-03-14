391ce9e5245f025a62742f083584a0722c131aee
Vinegar (Flynn Waiver) <124418090+moxie-coder@users.noreply.github.com>
2026-03-13 20:02:07 -0400

    Revert "Remove Windows resource (.rc) and icon handling"
    This reverts commit 9544812b00b8d4c19a4bbc0095be4602c6de69ac.

--- tools/platforms/WindowsPlatform.hx
+++ tools/platforms/WindowsPlatform.hx
@@ -316,8 +316,8 @@ class WindowsPlatform extends PlatformTarget
 		}
 		else
 		{
-			var haxeArgs = [hxml];
-			var flags = [];
+			var haxeArgs = [hxml, "-D", "resourceFile=ApplicationMain.rc"];
+			var flags = ["-DresourceFile=ApplicationMain.rc"];
 
 			if (is64)
 			{
@@ -342,6 +342,8 @@ class WindowsPlatform extends PlatformTarget
 				System.runCommand("", "haxe", haxeArgs);
 
 				if (noOutput) return;
+
+				IconHelper.createWindowsIcon(icons, Path.combine(targetDirectory + "/obj", "ApplicationMain.ico"));
 
 				CPPHelper.compile(project, targetDirectory + "/obj", flags);
 
@@ -370,6 +372,8 @@ class WindowsPlatform extends PlatformTarget
 				System.runCommand("", "haxe", haxeArgs.concat(["-D", "static_link"]));
 
 				if (noOutput) return;
+
+				IconHelper.createWindowsIcon(icons, Path.combine(targetDirectory + "/obj", "ApplicationMain.ico"));
 
 				CPPHelper.compile(project, targetDirectory + "/obj", flags.concat(["-Dstatic_link"]));
 
@@ -607,9 +611,14 @@ class WindowsPlatform extends PlatformTarget
 		ProjectHelper.recursiveSmartCopyTemplate(project, "haxe", targetDirectory + "/haxe", context);
 		ProjectHelper.recursiveSmartCopyTemplate(project, targetType + "/hxml", targetDirectory + "/haxe", context);
 
-		if (targetType == "cpp" && project.targetFlags.exists("static"))
+		if (targetType == "cpp")
 		{
-			ProjectHelper.recursiveSmartCopyTemplate(project, "cpp/static", targetDirectory + "/obj", context);
+			ProjectHelper.recursiveSmartCopyTemplate(project, "windows/resource", targetDirectory + "/obj", context);
+
+			if (project.targetFlags.exists("static"))
+			{
+				ProjectHelper.recursiveSmartCopyTemplate(project, "cpp/static", targetDirectory + "/obj", context);
+			}
 		}
 
 		/*if (IconHelper.createIcon (project.icons, 32, 32, Path.combine (applicationDirectory, "icon.png"))) {
